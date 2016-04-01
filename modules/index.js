b59a8a08e2ee25cda5e97e8961a6166c2293fa55
Mbob023 <armcgrathmedia@gmail.com>
2016-04-01 14:33:15 -0500

    initial commit

---
+++ modules/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+// DB STUFF
+
+router.get('/*', function(request, response) {
+  var file = request.params[0] || '/views/index.html';
+  response.sendFile(path.join(__dirname, '/public', file));
+});
+
+module.exports = router;
